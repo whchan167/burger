d7e824bdc7d966003101f0441c771b58c541aa01
wen <wen@wens-mbp.attlocal.net>
2017-02-12 22:18:21 -0600

    changes

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connectioninfo = {
   port: 3306,
   host: "localhost",
   user: "root",
-  password: "Wc041677",
+  password: "",
   database: "burgers_db"
-});
+}
+
+var connection = mysql.createConnection(connectioninfo);
+
+if (process.env.JAWSDB_URL) {
+  connectioninfo = process.env.JAWSDB_URL
+}
 
 // Make connection.
 connection.connect(function(err) {
